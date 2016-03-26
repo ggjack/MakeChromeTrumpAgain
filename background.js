45df0ca60d1a61e8670f0d9f64ae7025d858e497
ggjack <gg.jack@hotmail.com>
2016-03-26 10:21:03 -0400

    Added js background file

---
+++ background.js
@@ -0,0 +1,18 @@
+var state=false;
+var url=window.location.href;
+
+function toggle(){
+	state=!state;
+	if(state){
+
+	}
+	else{
+
+	}
+}
+
+
+chrome.browserAction.onClicked.addListener(toggle);
+
+
+
