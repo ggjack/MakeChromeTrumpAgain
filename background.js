a9fdf538dd2439c6dfee92213a4efe8629bfa1fb
ggjack <gg.jack@hotmail.com>
2016-03-26 12:28:42 -0400

    Added icons to manifest and working on background.js

--- background.js
+++ background.js
@@ -1,15 +1,32 @@
 var state=false;
 var url=window.location.href;
+url.toLoweCase();
+var followImg= document.createElement("img");
+img.src="images/trump.png"
+
+var src=document.getElementById("header");
 
 function toggle(){
 	state=!state;
 	if(state){
-
+		chrome.browserAction.setIcon({path: "icon128.png"});		
 	}
 	else{
-
+		chrome.browserAction.setIcon({path: "icon128off.png"});
 	}
 }
+
+
+if (state && !url.includes("trump")){
+
+}
+
+$(document).mousemove(function(e) {
+    	followImg.offset({
+        left: e.pageX,
+        top: e.pageY
+    });
+});
 
 
 chrome.browserAction.onClicked.addListener(toggle);
